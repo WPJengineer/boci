6f95db89e8ccbbaba19a49b9fa2fccbc27d0caae
WPJengineer <jkelly20940@iesjoanramis.org>
2026-03-20 10:43:44 +0100

    alter images in about us page, change shpping cart button to be fixed and bottom right of the screen, alter where seeearch bar is located

--- backend/endpoints/product_frontend.php
+++ backend/endpoints/product_frontend.php
@@ -2,20 +2,34 @@
 
 header("Content-Type: application/json");
 
+// if (!isset($_GET['id']) || $_GET['id'] === '') {
+//     echo json_encode(["error" => "Missing product id"]);
+//     exit;
+// }
+
+// $product_id = intval($_GET['id']);
+
 // this shows all products like in the products home page
 $sql = "SELECT *
-FROM boci_products
-;";
+FROM `boci_products`";
 
 // this shows a specific product if we select it from the products home page
 if (isset($_GET['id']) && $_GET['id'] !== '') {
-  $product_id = $_GET['id'];
+  $product_id = intval($_GET['id']);
   $sql .= " WHERE product_id = $product_id";
 }
 
 include('../config/db_config.php');
 
 $result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    echo json_encode([
+        "error" => mysqli_error($conn),
+        "query" => $sql
+    ]);
+    exit;
+}
 
 $products = [];
 
