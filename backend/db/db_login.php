f15320737e08ced9bfb9dc9c9adb10efc04c7ec3
WPJengineer <jkelly20940@iesjoanramis.org>
2026-03-20 14:42:49 +0100

    added logic to track login

---
+++ backend/db/db_login.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start()
+
+$email = htmlspecialchars($_POST['email']);
+$password = htmlspecialchars($_POST['password']);
+$backend = $_SERVER['DOCUMENT_ROOT'].'/boci/backend';
+
+include('../config/db_config.php');
+
+$sql = "SELECT *
+FROM `boci_customers`
+WHERE email = '$email' AND
+password = '$password';";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION['customer_id'] = $row['customer_id'];
+    $_SESSION['username'] = $row['forename'];
+    $_SESSION['userLastname'] = $row['lastname'];
+    $_SESSION['customer_role'] = $row['customer_role'];
+
+    //option for redirection for admin vs customer later.
+    header("Location: /boci/index.html");
+    exit();
+    
+  } else  {
+      echo "No customer found with that username and/or password";
+  }
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
